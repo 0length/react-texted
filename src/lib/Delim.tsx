a1b2706ac12f2c1100d3ce83fb3c86e2770cac88
0length <fajarjunyor13@gmail.com>
2020-06-27 15:34:22 +0700

    create delimiter component

---
+++ src/lib/Delim.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+interface DelimIProps {
+
+}
+const Delim: React.FC<DelimIProps> = ()=> {
+    return (<div className="reed-delim">
+        <div className="reed-delim__content"></div>
+    </div>)
+}
+
+export default Delim
